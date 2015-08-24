ada80e429ddae10e43c5319230595c2004f74f65
Jack Coleman <Jack.Carson.Coleman@gmail.com>
2015-08-24 14:40:13 -0700

    Update bootBot.js

--- bootBot.js
+++ bootBot.js
@@ -2,6 +2,4 @@
 // JavaScript file that boots code from development (C:/ etc.) platform
 //
 alert("boot is working for you");
-
-
-alert("boot has kicked thed bucket");
+//alert("boot has kicked thed bucket");
